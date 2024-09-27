d48e8a66a72cb7167533aefff8c86e4f2a9afb7a
Oliver Parr <oliverflynnyparr@gmail.com>
2024-09-27 17:09:07 +0100

    1

---
+++ main/loader.lua
@@ -0,0 +1,25 @@
+local L = {}
+local _collection = nil
+local village_ids = {"/sign"}
+
+local coords = {
+	village = vmath.vector3(385, 447, 0), -- 1
+	my_house_downstairs = vmath.vector3(), -- 2
+	my_house_upstairs = vmath.vector3()
+}
+
+function L.get_collection(collection)
+	_collection = collection
+end
+
+function L.give_collection()
+	return _collection
+end
+
+function L.collection_go_ids(collection)
+	if collection == "village" then
+		return village_ids
+	end
+end
+
+return L
---
+++ main/m_char/m_char.go
@@ -0,0 +1,49 @@
+components {
+  id: "m_char_script"
+  component: "/main/m_char/m_char.script"
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"m_char\"\n"
+  "mask: \"my_house_sign\"\n"
+  "mask: \"my_house\"\n"
+  "mask: \"exit_my_house\"\n"
+  "mask: \"barrier\"\n"
+  "mask: \"downstairs\"\n"
+  "mask: \"floor_2\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      y: -10.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 7.3333335\n"
+  "  data: 6.1593957\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/m_char/m_char.tilesource\"\n"
+  "}\n"
+  ""
+  position {
+    z: -1.5
+  }
+}
