9d70a412e023fb6760f9614e66256536bbb5190c
Oliver Parr <oliverflynnyparr@gmail.com>
2024-09-28 14:57:45 +0100

    back to where i was a week ago just better

--- main/loader.lua
+++ main/loader.lua
@@ -1,30 +1,12 @@
 local L = {}
 local _collection = nil
-local village_ids = {"/sign"}
 
-local coords = {
-	village = vmath.vector3(385, 447, 0), -- 1
-	my_house_downstairs = vmath.vector3(), -- 2
-	my_house_upstairs = vmath.vector3()
-}
-
-function L.get_collection(collection)
+function L.give_collection(collection)
 	_collection = collection
 end
 
-function L.give_collection()
+function L.get_collection()
 	return _collection
 end
 
-function L.collection_go_ids(collection)
-	if collection == "village" then
-		return village_ids
-	end
-end
-
-function L.give_coords(collection)
-	if collection == "village" then
-		return coords
-	end
-end
 return L
