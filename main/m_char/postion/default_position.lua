363435578fc35838323408bfdb311bbb0147f361
Oliver Parr <oliverflynnyparr@gmail.com>
2024-10-02 17:20:31 +0100

    Tress

--- main/m_char/postion/default_position.lua
+++ main/m_char/postion/default_position.lua
@@ -1,14 +1,14 @@
 return {
 
 	position = {
-		village = vmath.vector3(385,447,0.9999),
-		my_house_downstairs = vmath.vector3(31,124,0.9999),
-		my_house_upstairs = vmath.vector3(133,32,0.9999)
+		village = vmath.vector3(385,447,0.9995),
+		my_house_downstairs = vmath.vector3(31,124,0.9995),
+		my_house_upstairs = vmath.vector3(133,32,0.9995)
 	},
 
 	player = {
 		collection = "#my_house_upstairs",
-		position = vmath.vector3(133,32,0.9999)
+		position = vmath.vector3(133,32,0.9995)
 	}
 
 }
