ee8ef96f90528725f50bcfe0277660aeabfdf8cd
Oliver Parr <oliverflynnyparr@gmail.com>
2024-10-08 19:16:34 +0100

    wepon store in here now

--- main/default.lua
+++ main/default.lua
@@ -3,7 +3,8 @@ return {
 	position = {
 		village = vmath.vector3(385,447,0.9995),
 		my_house_downstairs = vmath.vector3(31,124,0.9995),
-		my_house_upstairs = vmath.vector3(133,32,0.9995)
+		my_house_upstairs = vmath.vector3(133,32,0.9995),
+		weapon_store = vmath.vector3(46, 10, 0.9995)
 	},
 
 	player = {
@@ -14,7 +15,8 @@ return {
 
 	camera = {
 		my_house_downstairs = vmath.vector3(96, 120, 0),
-		my_house_upstairs = vmath.vector3(96, 96, 0)
+		my_house_upstairs = vmath.vector3(96, 96, 0),
+		weapon_store = vmath.vector3(72, 88, 0)
 	}
 
 }
