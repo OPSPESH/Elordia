c42d34c6f6c3411e028ac5313706dac6627cc984
Oliver Parr <oliverflynnyparr@gmail.com>
2024-10-17 19:28:05 +0100

    Printer in

---
+++ assets/village/npc/dialogue/styles.lua
@@ -0,0 +1,15 @@
+return {
+	default = {
+		font_height = 28,
+		spacing = 0.5,
+		scale = 1,
+		waving = false,
+		color = "#FFFFFF",
+		speed = 0.05,
+		appear = 0.01,
+		shaking = 0,
+		sound = "letter",
+		can_skip = true,
+		shake_on_write = 0,
+	}
+}
