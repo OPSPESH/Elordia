1c4b8c9ac24edc977f70d9b42b9e9efa53575ce0
Oliver Parr <oliverflynnyparr@gmail.com>
2024-10-29 17:30:34 +0000

    INVENTORY

--- main/default.lua
+++ main/default.lua
@@ -1,5 +1,4 @@
 return {
-
 	position = {
 		village = vmath.vector3(385,447,0.9995),
 		my_house_downstairs = vmath.vector3(31,124,0.9995),
@@ -13,73 +12,39 @@ return {
 		weapon_store = vmath.vector3(72, 88, 0)
 	},
 
-	items = {
-		SHORTSWORD = {
-			buy = 10,
-			sell = 4,
-			damage = 6,
-			range = 5,
-			tooltip = "Basic sword. Will never fail you."
-		},
-
-		WOODENSWORD = {
-			buy = 2,
-			sell = 1,
-			damage = 1,
-			range = 5,
-			tooltip = "Cheap and nasty."
-		},
-
-		DAGGER = {
-			buy = 5,
-			sell = 2,
-			damage = 3,
-			range = 2,
-			tooltip = "Nice and small. Easily consealed."
-		},
-
-		BOW = {
-			buy = 8,
-			sell = 3,
-			damage = 16,
-			range = 100,
-			tooltip = "Basic bow. Good for hunting"
-		},
-		CROSSBOW = {
-			buy = 10,
-			sell = 3,
-			damage = 20,
-			range = 45,
-			tooltip = "Basic crossbow."
-		},
-		ARROW_5 = {
-			buy = 3,
-			sell = 1,
-		},
-		ARROW_10 = {
-			buy = 6,
-			sell = 2,
-		},
-
-		BOLT_5 = {
-			buy = 4,
-			sell = 1,
-		},
-
-		BOLT_10 = {
-			buy = 8,
-			sell = 2,
-		}
-	},
-
 	player = {
 		collection = "#my_house_upstairs",
 		position = vmath.vector3(133,32,0.9995),
 		sprint = false,
+	},
+
+	inventory = {
+		s0 = "nul",
+		s1 = "nul",
+		s2 = "nul",
+		s3 = "nul",
+		s4 = "nul",
+		s5 = "nul",
+		s6 = "nul",
+		s7 = "nul",
+		s8 = "nul",
+		s9 = "nul",
+		s10 = "nul",
+		s11 = "nul",
+		s12 = "nul",
+		s13 = "nul",
+		s14 = "nul",
+		s15 = "nul",
+		s16 = "nul",
+		s17 = "nul",
+		s18 = "nul",
+		s19 = "nul",
+		s20 = "nul",
+		s21 = "nul",
+		s22 = "nul",
+		s23 = "nul",
 		gold = 10,
 		starshard = 110,
-		inventory = {
-		}
 	},
 
 	npc = {
